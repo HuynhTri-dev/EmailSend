8ee652d64437d1aafa5c8c2ed1a70a7223ccbdfd
TriHuynh <huynhminhtri1109@gmail.com>
2024-12-28 08:29:53 +0700

    Add project files.

---
+++ EmailSend/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace EmailSend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recieve_txt = new System.Windows.Forms.TextBox();
+            this.sender_txt = new System.Windows.Forms.TextBox();
+            this.password_txt = new System.Windows.Forms.TextBox();
+            this.content = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // recieve_txt
+            // 
+            this.recieve_txt.Location = new System.Drawing.Point(244, 39);
+            this.recieve_txt.Name = "recieve_txt";
+            this.recieve_txt.Size = new System.Drawing.Size(272, 22);
+            this.recieve_txt.TabIndex = 0;
+            // 
+            // sender_txt
+            // 
+            this.sender_txt.Location = new System.Drawing.Point(244, 83);
+            this.sender_txt.Name = "sender_txt";
+            this.sender_txt.Size = new System.Drawing.Size(272, 22);
+            this.sender_txt.TabIndex = 1;
+            // 
+            // password_txt
+            // 
+            this.password_txt.Location = new System.Drawing.Point(244, 131);
+            this.password_txt.Name = "password_txt";
+            this.password_txt.Size = new System.Drawing.Size(272, 22);
+            this.password_txt.TabIndex = 2;
+            // 
+            // content
+            // 
+            this.content.Location = new System.Drawing.Point(244, 182);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(272, 117);
+            this.content.TabIndex = 3;
+            this.content.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(435, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.password_txt);
+            this.Controls.Add(this.sender_txt);
+            this.Controls.Add(this.recieve_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox recieve_txt;
+        private System.Windows.Forms.TextBox sender_txt;
+        private System.Windows.Forms.TextBox password_txt;
+        private System.Windows.Forms.RichTextBox content;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EmailSend/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmailSend")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmailSend")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0371f46b-9c7a-4f21-aae4-2e788b7a57fb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
